5cbed1a6fcd67009d05ce7eb56fd678b0225fee9
josephernest <nouvellecollection@gmail.com>
2015-06-17 00:11:34 +0200

    minor update

--- samplerbox.py
+++ samplerbox.py
@@ -20,7 +20,7 @@ SAMPLES_DIR = "."                       # The root directory containing the samp
 USE_SERIALPORT_MIDI = False             # Set to True to enable MIDI IN via SerialPort (e.g. RaspberryPi's GPIO UART pins)
 USE_I2C_7SEGMENTDISPLAY = False         # Set to True to use a 7-segment display via I2C 
 USE_BUTTONS = False                     # Set to True to use momentary buttons (connected to RaspberryPi's GPIO pins) to change preset
-MAX_POLYPHONY = 80                      # This can be set higher, but 100 is a good value
+MAX_POLYPHONY = 80                      # This can be set higher, but 80 is a safe value
 
 
 #########################################

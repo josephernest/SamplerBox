68344f1ee0e01917e394a632de956da7cccdc68e
Paul T <iam@ptitov.ru>
2016-02-07 06:38:33 +0000

    Change GCC settings to build legacy library correctly

--- Makefile
+++ Makefile
@@ -1,3 +1,2 @@
-samplerbox_audio.so: export CFLAGS=-mcpu=cortex-a7 -mtune=arm1176jzf-s -mfloat-abi=hard -mfpu=neon-vfpv4 -ftree-vectorize -ffast-math -O3
 samplerbox_audio.so: samplerbox_audio.pyx samplerbox_audio_neon.pyx
 	python setup.py build_ext --inplace
--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ import numpy
 
 extensions = [
     Extension("samplerbox_audio", ["samplerbox_audio.pyx"]),
-    Extension("samplerbox_audio_neon", ["samplerbox_audio_neon.pyx"]),
+    Extension("samplerbox_audio_neon", ["samplerbox_audio_neon.pyx"],
+        extra_compile_args=["-mcpu=cortex-a7", "-mtune=arm1176jzf-s", "-mfloat-abi=hard", "-mfpu=neon-vfpv4", "-ftree-vectorize", "-ffast-math", "-O3"]),
 ]
 
 setup(ext_modules = cythonize(extensions), include_dirs=[numpy.get_include()])

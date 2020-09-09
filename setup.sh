5fc31bd631b737cba40ab602b3c49d6a695439af
devegoo <57900186+devegoo@users.noreply.github.com>
2020-09-09 14:56:36 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /opt/
+git clone https://github.com/devegoo/SamplerBox.git
+cd SamplerBox
+apt-get update ; apt-get -y install git python3-dev python3-pip python3-numpy cython3 python3-smbus portaudio19-dev libportaudio2 libffi-dev : pip3 install rtmidi-python pyaudio cffi sounddevice future
+python3 setup.py build_ext --inplace
+cd tools/
+./install.sh
+
+
+

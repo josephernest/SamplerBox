de9c8672fa22187db233d49e9a4576506b86b7e1
devegoo <57900186+devegoo@users.noreply.github.com>
2020-09-09 15:28:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,13 @@ git clone https://github.com/devegoo/SamplerBox.git
 cd SamplerBox
 apt-get update ; apt-get -y install git python3-dev python3-pip python3-numpy cython3 python3-smbus portaudio19-dev libportaudio2 libffi-dev : pip3 install rtmidi-python pyaudio cffi sounddevice future
 python3 setup.py build_ext --inplace
-cd tools/
-chmod +x install.sh
-./install.sh
+#services / control
+ct /tools/
+cp samplerbox.service /etc/systemd/system/samplerbox.service
+ln -s start_samplerbox /usr/bin/
+ln -s stop_samplerbox /usr/bin/
+# sample renamer
+ln -s nametonote /usr/bin/
 
 
 

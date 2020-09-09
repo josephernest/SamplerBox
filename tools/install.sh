3953f87a141a2f1ea2aa69004b0c05cd7935a873
devegoo <57900186+devegoo@users.noreply.github.com>
2020-09-09 15:29:17 +0200

    Delete install.sh

--- tools/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#services / control
-cp samplerbox.service /etc/systemd/system/samplerbox.service
-ln -s start_samplerbox /usr/bin/
-ln -s stop_samplerbox /usr/bin/
-
-# sample renamer
-ln -s nametonote /usr/bin/
-

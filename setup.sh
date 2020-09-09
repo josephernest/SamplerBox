a142da23a031d7f58d2616be5c77100a473763e0
devegoo <57900186+devegoo@users.noreply.github.com>
2020-09-09 15:20:22 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ cd SamplerBox
 apt-get update ; apt-get -y install git python3-dev python3-pip python3-numpy cython3 python3-smbus portaudio19-dev libportaudio2 libffi-dev : pip3 install rtmidi-python pyaudio cffi sounddevice future
 python3 setup.py build_ext --inplace
 cd tools/
+chmod +x install.sh
 ./install.sh
 
 

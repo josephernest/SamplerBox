eaf039edbe2fce03af4151bcd992dfdb2355e2a4
devegoo <57900186+devegoo@users.noreply.github.com>
2020-09-09 15:43:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,10 @@ python3 setup.py build_ext --inplace
 #services / control
 ct /tools/
 cp samplerbox.service /etc/systemd/system/samplerbox.service
-ln -s start_samplerbox /usr/bin/
-ln -s stop_samplerbox /usr/bin/
+cp start_samplerbox /usr/bin/
+cp stop_samplerbox /usr/bin/
 # sample renamer
-ln -s nametonote /usr/bin/
+cp nametonote /usr/bin/
 chmod +x /usr/bin/start_samplerbox
 chmod +x /usr/bin/stop_samplerbox
 chmod +x /usr/bin/nametonote

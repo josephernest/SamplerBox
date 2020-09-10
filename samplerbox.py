6486082da6fa400d0c7ffe38d07c1da1bf366705
devegoo <57900186+devegoo@users.noreply.github.com>
2020-09-10 15:25:41 +0200

    Update samplerbox.py

--- samplerbox.py
+++ samplerbox.py
@@ -15,7 +15,7 @@
 #########################################
 
 AUDIO_DEVICE_ID = 0                     # change this number to use another soundcard
-SAMPLES_DIR = "/home/ts/SamplerBox/"                       # The root directory containing the sample-sets. Example: "/media/" to look for samples on a USB stick / SD card
+SAMPLES_DIR = "."                       # The root directory containing the sample-sets. Example: "/media/" to look for samples on a USB stick / SD card
 USE_SERIALPORT_MIDI = False             # Set to True to enable MIDI IN via SerialPort (e.g. RaspberryPi's GPIO UART pins)
 USE_I2C_7SEGMENTDISPLAY = False         # Set to True to use a 7-segment display via I2C
 USE_BUTTONS = False                     # Set to True to use momentary buttons (connected to RaspberryPi's GPIO pins) to change preset

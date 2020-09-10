6476d54fd6cafd4ec67c94770bd56e868e820370
devegoo <57900186+devegoo@users.noreply.github.com>
2020-09-10 22:15:29 +0200

    Create local_hd_Install.sh

---
+++ local_hd_Install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ..
+mv SammplerBox /opt/
+cd /opt/SamplerBox
+apt-get update ; apt-get -y install git python3-dev python3-pip python3-numpy cython3 python3-smbus portaudio19-dev libportaudio2 libffi-dev ; pip3 install rtmidi-python pyaudio cffi sounddevice future
+python3 setup.py build_ext --inplace
+#services / control
+cd tools
+cp samplerbox.service /etc/systemd/system/samplerbox.service
+cp start_samplerbox /usr/bin/
+cp stop_samplerbox /usr/bin/
+# sample renamer
+cp nametonote /usr/bin/
+chmod +x /usr/bin/start_samplerbox
+chmod +x /usr/bin/stop_samplerbox
+chmod +x /usr/bin/nametonote

f1a33fb2c894ee555918dfb597831a78c6c62de4
devegoo <57900186+devegoo@users.noreply.github.com>
2020-09-11 11:39:31 +0200

    Update local_hd_Install.sh

--- local_hd_Install.sh
+++ local_hd_Install.sh
@@ -2,7 +2,9 @@
 cd ..
 mv SammplerBox /opt/
 cd /opt/SamplerBox
-apt-get update ; apt-get -y install git python3-dev python3-pip python3-numpy cython3 python3-smbus portaudio19-dev libportaudio2 libffi-dev ; pip3 install rtmidi-python pyaudio cffi sounddevice future
+chmod +x install_requeriments
+./install_requeriments
+pip3 install -r requirements.txt
 python3 setup.py build_ext --inplace
 #services / control
 cd tools

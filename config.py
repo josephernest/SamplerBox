f692af0693e4e47f48a20cb6b435a0cf3c416d89
josephernest <nouvellecollection@gmail.com>
2022-08-09 12:20:30 +0200

    Python 3 support (requires 3.7), and various modifications, included some from PR #47

---
+++ config.py
@@ -0,0 +1,12 @@
+#########################################
+# LOCAL
+# CONFIG
+#########################################
+
+AUDIO_DEVICE_ID = 2                     # change this number to use another soundcard
+SAMPLES_DIR = "."                       # The root directory containing the sample-sets. Example: "/media/" to look for samples on a USB stick / SD card
+USE_SERIALPORT_MIDI = False             # Set to True to enable MIDI IN via SerialPort (e.g. RaspberryPi's GPIO UART pins)
+USE_I2C_7SEGMENTDISPLAY = False         # Set to True to use a 7-segment display via I2C
+USE_BUTTONS = False                     # Set to True to use momentary buttons (connected to RaspberryPi's GPIO pins) to change preset
+MAX_POLYPHONY = 80                      # This can be set higher, but 80 is a safe value
+USE_SYSTEMLED = False                   # Flashing LED after successful boot, only works on RPi/Linux

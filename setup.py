f9441d12e06d867f9cf2e1e2b0741ea9c540a16e
dosas <dosas@github.com>
2024-04-23 19:26:02 +0200

    Make it pip installable

--- setup.py
+++ setup.py
@@ -1,5 +1,33 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
+from setuptools import setup, Extension
 
-setup(ext_modules = cythonize("samplerbox_audio.pyx"), include_dirs=[numpy.get_include()])
+
+def my_build_ext(pars):
+    from setuptools.command.build_ext import build_ext as _build_ext
+
+    class build_ext(_build_ext):
+        def finalize_options(self):
+            _build_ext.finalize_options(self)
+            __builtins__.__NUMPY_SETUP__ = False
+            import numpy
+            self.include_dirs.append(numpy.get_include())
+
+    return build_ext(pars)
+
+
+setup(ext_modules=[Extension(name="samplerbox_audio",
+                             sources=["samplerbox_audio.pyx"])],
+      cmdclass={'build_ext': my_build_ext},
+      setup_requires=[
+          "cython",
+          "numpy"
+      ],
+      install_requires=[
+          'cffi',
+          'numpy',
+          'pyserial',
+          'python-rtmidi; python_version >= "3.9"',
+          'rtmidi-python; python_version < "3.9"',
+          'smbus',
+          'sounddevice',
+      ],
+      )
